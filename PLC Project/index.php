9607c1e1f72b73002de2ad2c2735dc541805fb3d
Akib Kazi <akazi05@nyit.edu>
2023-11-13 20:43:11 -0500

    Added files

--- PLC Project/index.php
+++ PLC Project/index.php
@@ -1,12 +1,18 @@
-<?php 
-include 'header.php';
-include 'sidebar.php';
-?>
+<img src="body_outline.webp" alt="" usemap="#image-map" class="maparea"/>
+<map name="image-map" id="image-map"></map>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Anatomical Fitness</title>
-</head>
+
+<!-- Your image map container -->
+<div id="image-container">
+    <!-- Your image map -->
+    <img src="body_outline.jpg" alt="logo" title="logo" width="700" height="650" usemap="#image-map">
+
+    <map name="image-map">
+        <!-- Define clickable area in the top-left corner -->
+        <area shape="rect" coords="0,0,100,100" alt="Clickable Area" href="https://example.com">
+        <!-- Add more areas as needed -->
+    </map>
+</div>
+
+</body>
 </html>
-
