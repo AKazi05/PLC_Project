7df4de287d29e47a02d37a9158e50e68a1a322db
anuragcar <akar01@nyit.edu>
2023-11-26 22:40:29 -0500

    add vids and descriptions for glutes

--- PLC Project/gluteus_maximus.php
+++ PLC Project/gluteus_maximus.php
@@ -57,28 +57,28 @@
         //Workouts
 
         <form method="post" action="add_workout.php">
-            <li><strong>1. Preacher Curls:</strong> Use a preacher bench to isolate the biceps. Curl the weight up with your arms resting on the bench.
-                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Preacher Curls">Add to Workout</button>
+            <li><strong>1. Split Squats</strong> Stand with one foot in front and one behind (on an elevated surface preferably). Lower your body by bending your knees until your rear knee is almost touching the ground. Push through the front heel to return to the starting position. Switch the legs and repeat.
+                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Split Squats">Add to Workout</button>
             </li>
         </form>
 
-        <iframe width="480" height="270" src="https://www.youtube.com/embed/ykJmrZ5v0Oo" frameborder="0" allowfullscreen></iframe>
+        <iframe width="480" height="270" src="https://www.youtube.com/watch?v=jNihW0WDIL4" frameborder="0" allowfullscreen></iframe>
 
         <form method="post" action="add_workout.php">
-            <li><strong>2. Preacher Curls:</strong> Use a preacher bench to isolate the biceps. Curl the weight up with your arms resting on the bench.
-                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Preacher Curls">Add to Workout</button>
+            <li><strong>2. Lunges</strong> Start standing with feet together. Step forward with one leg, and lower your body until both knees are at 90-degree angles, then push back up to the starting position. Repeat with the other leg.
+                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Lunges">Add to Workout</button>
             </li>
         </form>
 
-        <iframe width="480" height="270" src="https://www.youtube.com/embed/zC3nLlEvin4" frameborder="0" allowfullscreen></iframe>
+        <iframe width="480" height="270" src="https://www.youtube.com/watch?v=eFWCn5iEbTU" frameborder="0" allowfullscreen></iframe>
 
         <form method="post" action="add_workout.php">
-            <li><strong>3. Preacher Curls:</strong> Use a preacher bench to isolate the biceps. Curl the weight up with your arms resting on the bench.
-                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Preacher Curls">Add to Workout</button>
+            <li><strong>3. Hip Thrusts</strong> Sit on the floor with your upper back against a bench. Roll a barbell over your hips, bend your knees, and push through your heels to lift your hips until your body forms a straight line from knees to shoulders, squeezing your glutes at the top. Lower back down with control.
+                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Hip Thrusts">Add to Workout</button>
             </li>
         </form>
         //YouTube Video
-        <iframe width="480" height="270" src="https://www.youtube.com/embed/fIWP-FRFNU0" frameborder="0" allowfullscreen></iframe>
+        <iframe width="480" height="270" src="https://www.youtube.com/watch?v=EF7jXP17DPE" frameborder="0" allowfullscreen></iframe>
     </ul>
 </body>
 
