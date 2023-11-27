b7196cebf9ff2edc7eb1569ae38027aa6ee945ca
anuragcar <akar01@nyit.edu>
2023-11-26 22:19:44 -0500

    add exercise description for lats

--- PLC Project/latissimus_dorsi.php
+++ PLC Project/latissimus_dorsi.php
@@ -57,24 +57,24 @@
         //Workouts
 
         <form method="post" action="add_workout.php">
-            <li><strong>1. Preacher Curls:</strong> Use a preacher bench to isolate the biceps. Curl the weight up with your arms resting on the bench.
-                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Preacher Curls">Add to Workout</button>
+            <li><strong>1. Pullups</strong> 
+                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Pullups">Grab the bar with your preferred grip, hang, then pull yourself up until your chin passes the bar (pulling to the clavicle or sternum is optional), and lower yourself back down with control.</button>
             </li>
         </form>
 
         <iframe width="480" height="270" src="https://www.youtube.com/embed/ykJmrZ5v0Oo" frameborder="0" allowfullscreen></iframe>
 
         <form method="post" action="add_workout.php">
-            <li><strong>2. Preacher Curls:</strong> Use a preacher bench to isolate the biceps. Curl the weight up with your arms resting on the bench.
-                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Preacher Curls">Add to Workout</button>
+            <li><strong>2. Pulldowns</strong> Sit at the pulldown machine and adjust the thigh pad so that your thighs are comfortably secured under the pads. Grasp the bar your preferred grip, and pull it down towards your chest while keeping your back straight, then slowly return the bar to the starting position.
+                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Pulldowns">Add to Workout</button>
             </li>
         </form>
 
         <iframe width="480" height="270" src="https://www.youtube.com/embed/zC3nLlEvin4" frameborder="0" allowfullscreen></iframe>
 
         <form method="post" action="add_workout.php">
-            <li><strong>3. Preacher Curls:</strong> Use a preacher bench to isolate the biceps. Curl the weight up with your arms resting on the bench.
-                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Preacher Curls">Add to Workout</button>
+            <li><strong>3. Dumbbell Pullovers</strong> Lie on a bench, hold a dumbbell above your chest with both hands, then lower it backward over your head in a controlled arc until your upper arms are in line with your torso or slightly below. Then, bring the dumbbell back up over your chest to the starting position while keeping a slight bend in your elbows.
+                &nbsp&nbsp&nbsp&nbsp<button type="submit" name="exercise" value="Dumbbell Pullovers">Add to Workout</button>
             </li>
         </form>
         //YouTube Video
