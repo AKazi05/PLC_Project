6914de250bb5f64c62a6453bc4a979bee1a1eea5
anuragcar <akar01@nyit.edu>
2023-12-06 18:21:54 -0500

    add workout table

--- models.py
+++ models.py
@@ -4,6 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+class Workout(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    exercise = db.Column(db.String(100))
+    username = db.Column(db.String(256), db.ForeignKey('user.username'))  # references 'username' in 'User' table
+    user = db.relationship('User', backref=db.backref('workouts', lazy=True))
+    
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(320), unique=True, nullable=False)
